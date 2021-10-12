4acf25669cd706db6a76f96c6be420799c9b4cb0
Trachoma <ssehcn1024@outlook.com>
2021-10-12 16:48:44 +0800

    1. 缓存路由组件 	2. 路由组件懒加载 	3. 图片懒加载   4.使用moment实现日期过滤

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import App from './App'
 import { Button } from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
+import App from './App'
 import router from './router'
 import store from './store'
 
 // 加载mockServer
 import './mock/mockServer'
+import loading from './common/image/loading.gif'
+import './filters' // 加载过滤器
 
 Vue.config.productionTip = false
 // 注册全局组件标签
 Vue.component(Button.name, Button) // <mt-button>
+Vue.use(VueLazyload, {
+  loading: loading,
+})
 
 const vm = new Vue({
   render: h => h(App),
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Msite from '../pages/Msite/Msite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
+
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
 import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
 import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
+const Msite = () => import('../pages/Msite/Msite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
 
 Vue.use(VueRouter)
 
